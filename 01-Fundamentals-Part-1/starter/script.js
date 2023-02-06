daceab22dbf869b15964d98e18409650f5e3792c
Karol Olędzki <98383185+mocnykarolek@users.noreply.github.com>
2023-02-06 12:17:53 +0100

    Add files via upload

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,30 @@
+// //Coding challenge #1
+// const markWeight = 78;
+// const markHight = 1.69;
+// const johnWeight = 92;
+// const johnHight = 1.95;
+
+// const marksBMI = markWeight / markHight ** 2;
+// const johnsBMI = johnWeight / johnHight ** 2;
+
+// console.log(markWeight, markHight, marksBMI);
+// console.log(johnWeight, johnHight, johnsBMI);
+
+// let markHigherBMI = marksBMI > johnsBMI;
+// console.log(markHigherBMI);
+
+// //****************************Second data******************************//
+
+// const markWeight1 = 95;
+// const markHight1 = 1.88;
+// const johnWeight1 = 85;
+// const johnHight1 = 1.76;
+
+// const marksBMI1 = markWeight1 / markHight1 ** 2;
+// const johnsBMI1 = johnWeight1 / johnHight1 ** 2;
+
+// console.log(markWeight1, markHight1, marksBMI1);
+// console.log(johnWeight1, johnHight1, johnsBMI1);
+
+// let markHigherBMI1 = marksBMI1 > johnsBMI1;
+// console.log(markHigherBMI1);
